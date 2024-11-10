2658b232eaeb2bd76fda7a3904bfc6ee9f47e545
Hoa <hoameomu@gmail.com>
2024-11-10 22:28:56 +0800

    [bitutil] split Align into AlignU32 and AlignU64

--- bitutil/bit_util.go
+++ bitutil/bit_util.go
@@ -4,17 +4,19 @@ type Int interface {
 	int | uint | int8 | uint8 | int16 | uint16 | int32 | uint32 | int64 | uint64
 }
 
-type UInt interface {
-	uint | uint8 | uint16 | uint32 | uint64
-}
-
 // IsPowerOfTwo checks if a number is a power of two.
 func IsPowerOfTwo[T Int](value T) bool {
 	return value > 0 && ((value & (^value + 1)) == value)
 }
 
-// Align aligns a value to the next multiple of alignment.
+// AlignU32 aligns a value to the next multiple of alignment.
 // If the value is already a multiple of alignment, it returns the value unchanged.
-func Align(value, alignment int) int {
+func AlignU32(value, alignment uint32) uint32 {
+	return (value + (alignment - 1)) & -alignment
+}
+
+// AlignU64 aligns a value to the next multiple of alignment.
+// If the value is already a multiple of alignment, it returns the value unchanged.
+func AlignU64(value, alignment uint64) uint64 {
 	return (value + (alignment - 1)) & -alignment
 }
